0f72c9aa282fa7f619fc6a8bba300834cf3ca4b9
helen121027 <helen121027@gmail.com>
2023-01-19 06:55:49 +0900

    Feat: 라우팅 처리

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from  "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Board from "./pages/Board";
+import BoardCreate from "./pages/BoardCreate";
+import BoardDetail from "./pages/BoardDetail";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/board" element={<Board/>}/>
+          <Route path="/create" element={<BoardCreate/>}/>
+          <Route path="/board/:boardId" element={<BoardDetail/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
