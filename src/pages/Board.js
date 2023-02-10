9f19445f6aa7ef1188af6ce95d97ca34a5505670
KimMoHu <sanbonai0728@gmail.com>
2023-02-10 19:34:19 +0900

    브랜치 병합

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -3,20 +3,25 @@ import { useNavigate } from "react-router-dom";
 import Category from "../components/Board/category";
 import Header from "../components/Header";
 
-
 export default function Board() {
   const navigate = useNavigate();
-  
+
   const toCreatePage = () => {
-    navigate(`/create`)
-  }
+    navigate(`/create`);
+  };
 
   return (
     <div className="container">
       <Header />
       <h1 className="text-center mt-20 mb-10 font-bold text-4xl">게시판</h1>
       <div className="flex justify-end mr-[200px]">
-        <button type="button" onClick={toCreatePage} className='bg-main h-10 w-[100px] text-white rounded-md'>글 작성</button>
+        <button
+          type="button"
+          onClick={toCreatePage}
+          className="bg-main h-10 w-[100px] text-white rounded-md"
+        >
+          글 작성
+        </button>
       </div>
       <div className="mx-auto">
         {/* 카테고리 */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
     colors: {
-      'main' : '#AA233C',
-      transparent: 'transparent',
-      current: 'currentColor',
+      main: "#AA233C",
+      transparent: "transparent",
+      current: "currentColor",
       black: colors.black,
       white: colors.white,
       gray: colors.gray,
@@ -17,7 +17,5 @@ module.exports = {
       yellow: colors.yellow,
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+  plugins: [require("@tailwindcss/forms")],
 };
