8248df8d2d4d83697d7169eeb950526e73a0e94e
helen121027 <helen121027@gmail.com>
2023-02-14 18:12:57 +0900

    Feat : 게시판 카드 형식

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Category from "../components/Board/category";
-import Header from "../components/Header";
+import CardList from "../components/Board/CardList";
 
 
 export default function Board() {
@@ -13,15 +13,15 @@ export default function Board() {
 
   return (
     <div className="container">
-      <Header />
       <h1 className="text-center mt-20 mb-10 font-bold text-4xl">게시판</h1>
       <div className="flex justify-end mr-[200px]">
         <button type="button" onClick={toCreatePage} className='bg-main h-10 w-[100px] text-white rounded-md'>글 작성</button>
       </div>
-      <div className="mx-auto">
+      <div className="container px-5 py-10 mx-auto">
         {/* 카테고리 */}
         <Category />
         {/* 게시판 */}
+        <CardList />
       </div>
     </div>
   );
