cb4d755e4c7af3ffad88f86674f0c5655a054817
KimMoHu <sanbonai0728@gmail.com>
2023-02-16 05:56:43 +0900

    Feat: 게시글 상세 페이지 추가

--- src/pages/BoardDetail.js
+++ src/pages/BoardDetail.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import PostHeader from "../components/BoardDetail/PostHeader";
 import PostMain from "../components/BoardDetail/PostMain";
@@ -6,22 +6,28 @@ import PostComment from "../components/BoardDetail/PostComment";
 import { getPostById } from "../data/BoardDummyData";
 
 const BoardDetail = () => {
+  const [commentList, setCommentList] = useState([]); // 댓글 수를 postheader 에서 표시해야 하므로 전체 댓글 리스트는 props 끌어올리기를 사용, Board 페이지에서도 게시글 별 댓글 수를 표시하니 추후 Context 사용이 필요해보임
+
   const params = useParams();
   // getPostById 함수로 필터링한 게시글의 데이터를 구조 분해 할당한 후 각 컴포넌트에 props로 내려줌.
-  const { post_id, category, title, content, name, createDate, readCount } =
+  const { post_id, category, title, content, user, createDate, readCount } =
     getPostById(Number(params.boardId)); // params의 Id가 string으로 인식되어 getPostById 함수가 오작동하는 이슈 발견, Number 메소드로 파싱해주어 해결.
 
   return (
     <>
-      <div className="mx-[18%] mt-[5%] ">
+      <div className="max-w-3xl mx-auto mt-[5%] ">
         <PostHeader
           postId={post_id}
-          name={name}
-          createDate={createDate}
+          user={user}
+          postCreateDate={createDate}
           readCount={readCount}
+          commentList={commentList}
         />
         <PostMain title={title} content={content} category={category} />
-        <PostComment>댓글</PostComment>
+        <PostComment
+          commentList={commentList}
+          onUpdateCommentList={setCommentList}
+        />
       </div>
       <h1>{params.boardId}번 게시글 상세 페이지입니다.</h1>
     </>
