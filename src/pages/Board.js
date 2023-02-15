4b9bb3736a8d75a18d2fcff981f8aed3c1b1bb1f
KimMoHu <sanbonai0728@gmail.com>
2023-02-16 05:25:56 +0900

    Chore: 모달 관련 주석 추가

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -4,6 +4,16 @@ import Category from "../components/Board/category";
 import CardList from "../components/Board/CardList";
 
 export default function Board() {
+  // const [modalOpen, setModalOpen] = useState(false); // 모달 자체 컴포넌트는 하나만 생성하되, 사용처에 따라 내용을 다르게 보여줄 생각 따라서 추후 확장을 대비해 메인 페이지가 될 Board.js 에서 모달 오픈 상태를 관리하려 했으나...
+  // CardItem.js 에서 BoardDetail로 이동하는 방식이 컴포넌트 간 연결이 아닌 route 처리라 BoardDetail에서 props 끌어올리기로 통제 불가능, 모달 상태를 전역에서 관리하기 위해 컨텍스트를 사용하겠음
+  // const showModalHandler = () => {
+  //   setModalOpen(true);
+  // };
+
+  // const hideModalHandler = () => {
+  //   setModalOpen(false);
+  // };
+
   const navigate = useNavigate();
 
   const toCreatePage = () => {
