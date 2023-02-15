a98aa2e07a9f2d16c285d578ba602ef0599a832b
KimMoHu <sanbonai0728@gmail.com>
2023-02-16 05:59:44 +0900

    Feat: 게시글 상세 페이지 상단 헤더 컴포넌트 추가

--- src/components/BoardDetail/PostHeader.js
+++ src/components/BoardDetail/PostHeader.js
@@ -1,27 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import dayjs from "dayjs";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 import { faCommentDots } from "@fortawesome/free-regular-svg-icons";
 
 const PostHeader = (props) => {
+  const formattedPostCreateDate = dayjs(props.postCreateDate);
+  formattedPostCreateDate.format();
+
+  const postCreateYear = formattedPostCreateDate.get("y");
+  const postCreateMonth = formattedPostCreateDate.get("M");
+  const postCreateDay = formattedPostCreateDate.get("D");
+
   return (
-    <div className="flex justify-between">
+    <div className="w-auto flex justify-between">
       <Link to={"/"}>
         <img
-          className="w-[50px] h-[50px] rounded-full"
+          className="w-14 h-14 object-cover rounded-full"
           alt="유저 프로필 이미지"
+          src="https://picsum.photos/720/400"
         />
       </Link>
 
-      <div className="min-w-[70%]">
+      <div className="min-w-[60%] ml-4 flex flex-col justify-center">
         <div className="font-bold">{props.user}</div>
-        <div>{props.createDate}</div>
+        <div className="text-gray-400 text-subcontent">
+          {`${postCreateYear}년 ${postCreateMonth + 1}월 ${postCreateDay}일`}
+        </div>
       </div>
-      <div className="min-w-[20%] flex justify-between items-center">
-        <div>
-          <FontAwesomeIcon icon={faCommentDots} />
-          &nbsp;댓글 1
+      <div className="min-w-[30%] flex justify-evenly items-center">
+        <div className="flex justify-between items-center font-semiBold">
+          <FontAwesomeIcon icon={faCommentDots} className="mr-3 " />
+          댓글 {props.commentList.length}
         </div>
         <div>URL 복사</div>
         <FontAwesomeIcon
