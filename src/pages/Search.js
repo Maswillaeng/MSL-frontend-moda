8bf9e73e8019295e78ec3748c2f8dba75f35bcc7
helen121027 <helen121027@gmail.com>
2023-04-12 16:53:04 +0900

    Feat : 검색창 구현

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,7 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
 const Search = () => {
   return (
     <div>
-      <h1>검색 페이지입니다.</h1>
+      <form className="flex justify-center m-20">
+        <div className="relative">
+          <FontAwesomeIcon icon={faMagnifyingGlass} className="absolute w-5 h-5 text-slate-400 mt-3.5 inset-y-0 left-0 flex items-center pl-4 pointer-events-none"></FontAwesomeIcon>
+          <input className="block w-[450px] p-3 pl-12 rounded-lg border-slate-400 point" type="text" placeholder="검색"/>
+        </div>
+      </form>
     </div>
   )
 }
